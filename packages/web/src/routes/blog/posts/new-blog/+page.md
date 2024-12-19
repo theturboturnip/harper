a101e925a28f5835c01cb4df4e2276a4bc4d5096
Elijah Potter <me@elijahpotter.dev>
2024-12-19 13:00:03 -0700

    Revert "feat: basic implementation of blog posts"
    This reverts commit 27af3ed53ecbfa1f7b51a26eb3234e6b8acd0db8.

--- packages/web/src/routes/blog/posts/new-blog/+page.md
+++
@@ -1,5 +0,0 @@
----
-title: New Blog!
-description: abc
----
-# Ehmn this works?!
--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,19 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		vitePreprocess(),
-		mdsvex({
-			extensions: ['.md'],
-			layout: {
-				blog: 'src/routes/blog/posts/post.svelte'
-			}
-		})
-	],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
