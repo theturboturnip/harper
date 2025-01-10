0b4ff28600e87271308b7ee58801c962ff222381
Elijah Potter <me@elijahpotter.dev>
2025-01-10 13:29:52 -0700

    feat: autogenerate config with button

---
+++ packages/web/src/lib/DefaultNeovimConfig.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { LocalLinter } from 'harper.js';
+	import { CopyCode } from '@sveltepress/theme-default/components';
+	import { Button } from 'flowbite-svelte';
+
+	let linter = new LocalLinter();
+
+	let head = `lspconfig.harper_ls.setup {
+  settings = {
+    ["harper-ls"] = {
+      linters = {
+`;
+
+	let tail = `      }
+    }
+  },
+}`;
+
+	async function generateConfig() {
+		await linter.setLintConfigToDefault();
+		let config = await linter.getLintConfig();
+
+		let rows = Object.entries(config)
+			.map(([key, value]) => `\t${key} = ${value},`)
+			.reduce((prev, cur) => prev + '\n' + cur);
+
+		return head + rows + tail;
+	}
+
+	async function copyConfig() {
+		let defaultConfig = await generateConfig();
+		navigator.clipboard.writeText(defaultConfig);
+	}
+</script>
+
+<Button onclick={copyConfig}>Copy Default Config</Button>
