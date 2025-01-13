ce796556f0d3a6d27fc0206e0ae498f25257927e
Elijah Potter <me@elijahpotter.dev>
2025-01-13 14:08:04 -0700

    docs(harper.js): added comments to public types

--- packages/harper.js/src/main.ts
+++ packages/harper.js/src/main.ts
@@ -6,4 +6,7 @@ import WorkerLinter from './WorkerLinter';
 
 export { LocalLinter, WorkerLinter, SuggestionKind };
 export type { Linter, Lint, Span, Suggestion };
+
+/** A linting rule configuration dependent on upstream Harper's available rules.
+ * This is a record, since you shouldn't hard-code the existence of any particular rules and should generalize based on this struct. */
 export type LintConfig = Record<string, boolean | undefined>;
