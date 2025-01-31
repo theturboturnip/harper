b96da4b7a7c977e1eb281e49b69f43502b673e02
Elijah Potter <me@elijahpotter.dev>
2025-01-31 10:15:20 -0700

    feat(web): added a splash of color to the demo

--- harper-core/src/linting/lint_kind.rs
+++ harper-core/src/linting/lint_kind.rs
@@ -20,6 +20,24 @@ pub enum LintKind {
     Miscellaneous,
 }
 
+impl LintKind {
+    /// Produce a string representation, which can be used as keys in a map or CSS variables.
+    pub fn to_string_key(&self) -> String {
+        match self {
+            LintKind::Spelling => "Spelling",
+            LintKind::Capitalization => "Capitalization",
+            LintKind::Formatting => "Formatting",
+            LintKind::Repetition => "Repetition",
+            LintKind::Readability => "Readability",
+            LintKind::Miscellaneous => "Miscellaneous",
+            LintKind::Enhancement => "Enhancement",
+            LintKind::WordChoice => "WordChoice",
+            LintKind::Style => "Style",
+        }
+        .to_owned()
+    }
+}
+
 impl Display for LintKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = match self {
