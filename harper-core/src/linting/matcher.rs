d868a2f4c7e0955b1aa4d67e90d6320844035c65
hippietrail <hippytrail@gmail.com>
2025-02-11 18:00:48 +0800

    Implements #505 could of, should of, would of -> ... have
    [#505 feat: Flag "would of", "could of", and "should of", etc](https://github.com/Automattic/harper/issues/505)
    
    Supports negative and contracted versions.

--- harper-core/src/linting/matcher.rs
+++ harper-core/src/linting/matcher.rs
@@ -200,6 +200,18 @@ impl Matcher {
 
         // wrong set phrases and collocations
         triggers.extend(pt! {
+            "could", "of" => "could have",
+            "could", "of" => "could've",
+            "couldn't", "of" => "couldn't have",
+            "had", "of" => "had have",
+            "had", "of" => "had've",
+            "hadn't", "of" => "hadn't have",
+            "should", "of" => "should have",
+            "should", "of" => "should've",
+            "shouldn't", "of" => "shouldn't have",
+            "would", "of" => "would have",
+            "would", "of" => "would've",
+            "wouldn't", "of" => "wouldn't have",
             "same", "than" => "same as",
             "Same", "than" => "same as"
         });
