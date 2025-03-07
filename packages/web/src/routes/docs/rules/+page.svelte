9d318fdb125934b85953f6130a887a4f1f507921
Elijah Potter <me@elijahpotter.dev>
2025-03-07 09:05:25 -0700

    docs: add default state to rules page

--- packages/web/src/routes/docs/rules/+page.svelte
+++ packages/web/src/routes/docs/rules/+page.svelte
@@ -1,23 +1,27 @@
 <script module lang="ts">
-	import { LocalLinter } from 'harper.js';
+	import { LocalLinter, type LintConfig } from 'harper.js';
 
 	export const prerender = true;
 	export const frontmatter = {
 		title: 'Rules'
 	};
 
-	let info: Record<string, string> = $state({});
+	let descriptions: Record<string, string> = $state({});
+	let default_config: LintConfig = $state({});
 
 	let linter = new LocalLinter();
 	linter.getLintDescriptions().then(async (v) => {
-		info = v;
-		console.log(v);
+		descriptions = v;
+	});
+	linter.getDefaultLintConfig().then(async (v) => {
+		default_config = v;
 	});
 </script>
 
 <p>This page is an incomplete list of the various grammatical rules Harper checks for.</p>
 
-{#each Object.entries(info) as [name, description]}
+{#each Object.entries(descriptions) as [name, description]}
 	<h2>{name}</h2>
 	<p>{description}</p>
+	<p>This rule is {default_config[name] ? 'enabled' : 'disabled'} by default.</p>
 {/each}
