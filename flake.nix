6e0c807fdffaafa3a5d9962c82dca3c46086d42e
Al Duncanson <alDuncanson@proton.me>
2025-04-04 00:02:37 -0400

    feat(devshell): remove rust overlay

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs = {
     self,
     nixpkgs,
     utils,
-    rust-overlay,
   }:
     utils.lib.eachDefaultSystem (
       system: let
-        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
       in {
         devShell = with pkgs;
