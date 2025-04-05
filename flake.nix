0f7402b1bfa0f892d31a297f29deea0ef87dfc8b
Al Duncanson <alDuncanson@proton.me>
2025-04-05 01:05:54 -0400

    chore(devshell): reformat with nixfmt

--- flake.nix
+++ flake.nix
@@ -2,18 +2,22 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
     utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in {
-        devShell = with pkgs;
+      in
+      {
+        devShell =
+          with pkgs;
           mkShell {
             buildInputs = [
               just
