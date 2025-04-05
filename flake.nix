428a29b2a7c6698c722287fe48571f47039367ea
Al Duncanson <alDuncanson@proton.me>
2025-04-05 01:06:59 -0400

    feat(devshell): echo harper logo to terminal when dev environment loads

--- flake.nix
+++ flake.nix
@@ -31,6 +31,33 @@
               zip
               pandoc
             ];
+
+            shellHook = ''
+              echo "
+                                                                   YSOKGECAACDFIMRVZ               
+                                                                YQHAAAAABDFFDBAAAAABU              
+                                                              WKAAAFNTWZ      ZWTQNNY              
+                         ZTPMIFDCAAAAAEJPUZ                 ZKAADOX                                
+                      ZTJBAAABFHJMMMMLFAAAAGRZ             YFAAKZ                                  
+                     XDAAELSW          ZVQIAADQZ           QAAL                                    
+                     ZPNW                  VIAACOZ          VTZ                                    
+                          YPLIGDBBDFIKPY     VIABX                   YOKIFDABDGILPY                
+                        TJAAABEILLIFCAAAJT     WW                 ZSIAAACFIMLHEBAABKU              
+                      UEAADNW        XNEAADT                     SDAAEOX        WMDAAEV            
+                 YTTTNAACT              UDAAM                  ZLAAEV              TCAAOTTTY       
+                WBAAAAAFX                YGAAM                 KAAHY                XEAAAAACX      
+                YAAAAACZ                  ZEAAJMMMMMMIGIMMMMMMIAAFZ                  YBAAAAAZ      
+                YAAAAAK                    JAAAAAAAAAAAAAAAAAAAAAL                    IAAAAAZ      
+                 TNDAAT                    TAAAAAAAAAAAAAAAAAAAAAV                    RAAENU       
+                   KAAM                    NAAGTTTTTTRORTTTTTTFAAP                    KAAM         
+                   UAAC                    EAAU               RAAF                   ZBAAW         
+                    NAAHX                XIAAM                 KAAJY                WGAAO          
+                     OAABNX            XOBAAM                  ZLAACOY            XMAAAP           
+                      UEAAAGQVY    ZVQHAAAET                     SDAAAHRVZ    YVPGAAAFV            
+                        VNDAAAABEFBAAAADMV                         UMCAAAABFEBAAAAENW              
+                           VRPMKHHJMORV                               UROMJHHKMPRW
+              "
+            '';
           };
       }
     );
