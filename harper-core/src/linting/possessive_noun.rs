82a5bea1a77da617f6edefb3ed0986e21f570c03
Andrew Dunbar <hippytrail@gmail.com>
2025-07-23 04:09:52 +0700

    test: `#[ignore]`d failing test from issue #1582 (#1587)

--- harper-core/src/linting/possessive_noun.rs
+++ harper-core/src/linting/possessive_noun.rs
@@ -398,4 +398,44 @@ mod tests {
     fn allows_birds_hurried() {
         assert_lint_count("The birds hurried off.", test_linter(), 0);
     }
+
+    #[test]
+    #[ignore = "false positive issue 1582"]
+    fn allows_1582_harms_readability() {
+        assert_lint_count(
+            "This harms readability and maintainability.",
+            test_linter(),
+            0,
+        );
+    }
+
+    #[test]
+    #[ignore = "false positive issue 1582"]
+    fn allows_1582_imports_couples() {
+        assert_lint_count(
+            "Since using Webpack syntax in the imports couples the code to a module bundler",
+            test_linter(),
+            0,
+        );
+    }
+
+    #[test]
+    #[ignore = "false positive issue 1582"]
+    fn allows_1582_graphics_programmer() {
+        assert_lint_count(
+            "Are you a graphics programmer or Rust developer?",
+            test_linter(),
+            0,
+        );
+    }
+
+    #[test]
+    #[ignore = "false positive issue 1582"]
+    fn allows_1582_data_sources() {
+        assert_lint_count(
+            "these data sources can be queried using a full SQL dialect",
+            test_linter(),
+            0,
+        );
+    }
 }
