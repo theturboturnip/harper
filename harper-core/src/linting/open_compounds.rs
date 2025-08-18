1d04da89d4d5f7fdcc25ee59f353e647983d2467
Andrew Dunbar <hippytrail@gmail.com>
2025-08-19 04:00:00 +0900

    feat: separate joined "afew" and "infact" (#1755)

--- harper-core/src/linting/open_compounds.rs
+++ harper-core/src/linting/open_compounds.rs
@@ -1,6 +1,4 @@
-use crate::expr::Expr;
-use crate::expr::LongestMatchOf;
-use crate::expr::SequenceExpr;
+use crate::expr::{Expr, LongestMatchOf, SequenceExpr};
 use crate::{Lrc, Token, patterns::WordSet};
 
 use super::{ExprLinter, Lint, LintKind, Suggestion};
@@ -14,12 +12,14 @@ pub struct OpenCompounds {
 impl Default for OpenCompounds {
     fn default() -> Self {
         let phrases = [
+            "a few",
             "a lot",
             "a while",
             "as well",
             "at least",
             "each other",
             "in case",
+            "in fact",
             "in front",
         ];
         let mut compound_to_phrase = HashMap::new();
@@ -239,6 +239,17 @@ mod tests {
         );
     }
 
+    // A few
+
+    #[test]
+    fn correct_afew_atomic() {
+        assert_suggestion_result(
+            "ITK code to generate anisotropic metrics, mostly Riemannian metrics and afew particular cases of Finslerian metrics.",
+            OpenCompounds::default(),
+            "ITK code to generate anisotropic metrics, mostly Riemannian metrics and a few particular cases of Finslerian metrics.",
+        );
+    }
+
     // A lot
 
     #[test]
@@ -404,6 +415,17 @@ mod tests {
             "InCase save your secrets for a friend, so they can use in case it in case you went \"missing\".",
             OpenCompounds::default(),
             0,
+        );
+    }
+
+    // In fact
+
+    #[test]
+    fn correct_infact_atomic() {
+        assert_suggestion_result(
+            "Yes I do infact exist :O",
+            OpenCompounds::default(),
+            "Yes I do in fact exist :O",
         );
     }
 }
