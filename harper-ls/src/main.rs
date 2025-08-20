db887453ec0a50c10cbd7520034335e243892f90
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2025-08-20 18:15:12 +0000

    fix(harper-ls): remove version check (#1782)

--- harper-ls/src/main.rs
+++ harper-ls/src/main.rs
@@ -17,7 +17,7 @@ mod pos_conv;
 use backend::Backend;
 use clap::Parser;
 use tower_lsp_server::{LspService, Server};
-use tracing::{Level, error, info};
+use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 static DEFAULT_ADDRESS: &str = "127.0.0.1:4000";
@@ -32,9 +32,6 @@ struct Args {
     /// Set to listen on standard input / output rather than TCP.
     #[arg(short, long, default_value_t = false)]
     stdio: bool,
-    /// Skip the debug version check.
-    #[arg(long, default_value_t = false)]
-    skip_version_check: bool,
 }
 
 // Setting worker threads to four means the process will use about five threads total
@@ -52,10 +49,6 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let config = Config::default();
 
-    if !args.skip_version_check {
-        tokio::spawn(log_version_info());
-    }
-
     let (service, socket) = LspService::new(|client| Backend::new(client, config));
 
     if args.stdio {
@@ -71,32 +64,4 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-/// Ping Harper's website to get the latest available version.
-async fn get_latest_version() -> Result<String, reqwest::Error> {
-    let client = reqwest::Client::new();
-
-    client
-        .get("https://writewithharper.com/latestversion")
-        .header("Harper-Version", harper_core::core_version())
-        .send()
-        .await?
-        .error_for_status()?
-        .text()
-        .await
-}
-
-/// Log the current version information to the console.
-async fn log_version_info() {
-    match get_latest_version().await {
-        Ok(version) => info!("Latest available Harper version: {}", version),
-        Err(_err) => error!("Unable to obtain latest version."),
-    }
-
-    info!(
-        "Current harper-core version: {}",
-        harper_core::core_version()
-    );
-    info!("Current harper-ls version: {}", backend::ls_version());
 }
